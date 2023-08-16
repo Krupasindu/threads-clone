cd8d00fb0bf567ed9442317774f092318a84cde7
Krupasindu <krupasindusahoo@gmail.com>
2023-08-16 22:00:22 +0530

    Started adding componenets

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Topbar from "@/component/shared/Topbar";
+import LeftSiderbar from "@/component/shared/LeftSidebar";
+import RightSidebar from "@/component/shared/RightSidebar";
+import Bottombar from "@/component/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +22,18 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSiderbar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
---
+++ component/shared/LeftSidebar.tsx
@@ -0,0 +1,4 @@
+function LeftSiderbar() {
+  return <h1>LeftSiderbar</h1>;
+}
+export default LeftSiderbar;
