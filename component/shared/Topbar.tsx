0198b1dbe399401e1c20697bbe55a2cd97d98ed4
Krupasindu <krupasindusahoo@gmail.com>
2023-08-18 22:44:23 +0530

    Modified

--- component/shared/Topbar.tsx
+++ component/shared/Topbar.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 function Topbar() {
   return (
-    <nav className="topbar">
+    <nav>
       <Link href="/" className="flex items-center gap-4">
-        <Image src="/logo.svg" alt="logo" width={28} height={28} />
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
     </nav>
